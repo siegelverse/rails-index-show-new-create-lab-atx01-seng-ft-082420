38c05c720296075d5454c3cbd72bc0c013cfc3d5
siegelverse <msiegel37@gmail.com>
2020-09-22 08:44:39 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+c1 = Coupon.create(coupon_code: "56", store: "Target")
+c2 = Coupon.create(coupon_code: "4736379", store: "Best Buy")
+c3 = Coupon.create(coupon_code: "HSJ34DFJ", store: "HEB")
+c4 = Coupon.create(coupon_code: "sdkfjlskjlasg", store: "Cabelas")
---
+++ spec/factories/coupons.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :coupon do
+    name { "MyString" }
+    coupon_code { "MyString" }
+    store { "MyString" }
+  end
+end
